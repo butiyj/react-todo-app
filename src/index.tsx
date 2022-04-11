5d1c813d9fc85e61315050f7624e292e7633e0d8
butiyj <buti0523@gmail.com>
2022-04-12 01:23:40 +0900

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme";
+import { RecoilRoot } from "recoil";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
+);
+
+/*
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+  );
+*/
